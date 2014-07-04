e91478b88117a1b9151894b0c20cdf68845259cb
Steve Pear <post@tipue.com>
2014-07-04 16:46:18 +0100

    Version 4.0

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Tipue drop
 
 Tipue drop is a search suggestion box jQuery plugin.
 
-To get started, see <http://www.tipue.com/drop/>. There's a demo at <http://www.tipue.com/drop/demo/>.
+To get started, see <http://www.tipue.com/drop/>. There's also a demo.
 
 Documentation
 -------------
@@ -13,7 +13,7 @@ There's full documentation at <http://www.tipue.com/drop/>.
 Copyright and license
 ---------------------
 
-Tipue drop Copyright (c) 2013 Tipue, under the The MIT License.
+Tipue drop Copyright (c) 2014 Tipue, under the The MIT License.
 
 
 
--- tipuedrop/tipuedrop.js
+++ tipuedrop/tipuedrop.js
@@ -1,7 +1,7 @@
 
 /*
-Tipue drop 3.1
-Copyright (c) 2013 Tipue
+Tipue drop 4.0
+Copyright (c) 2014 Tipue
 Tipue drop is released under the MIT License
 http://www.tipue.com/drop
 */
@@ -14,7 +14,6 @@ http://www.tipue.com/drop
           var set = $.extend( {
           
                'show'                   : 3,
-               'maxWidth'               : 250,
                'speed'                  : 300,
                'newWindow'              : false,
                'mode'                   : 'static',
@@ -54,43 +53,41 @@ http://www.tipue.com/drop
                {
                     if ($obj.val())
                     {
-                         var out = '<div id="tipue_drop_point"></div>';
                          var c = 0;
                          for (var i = 0; i < tipuedrop_in.pages.length; i++)
                          {
                               var pat = new RegExp($obj.val(), 'i');
                               if ((tipuedrop_in.pages[i].title.search(pat) != -1 || tipuedrop_in.pages[i].text.search(pat) != -1 || tipuedrop_in.pages[i].tags.search(pat) != -1) && c < set.show)
                               {
+                                   if (c == 0)
+                                   {
+                                        var out = '<div id="tipue_drop_wrapper"><div class="tipue_drop_head"><div id="tipue_drop_head_text">Suggested results</div></div>';    
+                                   }
                                    out += '<a href="' + tipuedrop_in.pages[i].loc + '"';
                                    if (set.newWindow)
                                    {
                                         out += ' target="_blank"';
                                    }
-                                   out += '><div class="tipue_drop_item" style="max-width: ' + (set.maxWidth - 40) + 'px;"><img src="' + tipuedrop_in.pages[i].thumb + '" class="tipue_drop_image" alt="' + tipuedrop_in.pages[i].title + '">';
-                                   out += '<div class="tipue_drop_text">' + tipuedrop_in.pages[i].text + '</div></div></a>';
+                                   out += '><div class="tipue_drop_item"><div class="tipue_drop_left"><img src="' + tipuedrop_in.pages[i].thumb + '" class="tipue_drop_image"></div><div class="tipue_drop_right"><div class="tipue_drop_right_title">' + tipuedrop_in.pages[i].title + '</div><div class="tipue_drop_right_text">' + tipuedrop_in.pages[i].text + '</div></div></div></a>';
                                    c++;
                               }
                          }
-                         if (c == 0)
+                         if (c != 0)
                          {
-                              out += '<div class="tipue_drop_no_items">No suggestions</div>';     
+                              out += '</div>';               
+                              $('#tipue_drop_content').html(out);
+                              $('#tipue_drop_content').fadeIn(set.speed);
                          }
-                                        
-                         $('#tipue_drop_content').html(out);
-                         $('#tipue_drop_content').fadeIn(set.speed);
-                         $('#tipue_drop_fade').fadeIn(100);
                     }
                     else
                     {
                          $('#tipue_drop_content').fadeOut(set.speed);
-                         $('#tipue_drop_fade').fadeOut(100);
                     }
                }
                
                $('html').click(function()
                {
                     $('#tipue_drop_content').fadeOut(set.speed);
-                    $('#tipue_drop_fade').fadeOut(100);
                });
           
           });
